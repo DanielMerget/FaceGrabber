6b42713238645de916e5b01c445b5cc6e25dd8ed
MartinSchwörer <schwoere@in.tum.de>
2015-02-10 14:32:07 +0100

    fixed deadlock with multithreaded file writing

--- KinectCloudOutputWriter.cpp
+++ KinectCloudOutputWriter.cpp
@@ -34,7 +34,9 @@ static int numOfFilesToWrite = 100;
 void KinectCloudOutputWriter::pushCloud(pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloudToPush)
 {
 	std::unique_lock<std::mutex> cloudLocker(m_lockCloud);
-	if (m_cloudCount > numOfFilesToWrite || !m_running){
+	if (m_cloudCount >= numOfFilesToWrite || !m_running){
+		m_running = false;
+		m_checkCloud.notify_all();
 		return;
 	}
 	//PointCloudMeasurement cloudMeasurement{ cloudToPush, m_cloudCount };	
@@ -43,38 +45,47 @@ void KinectCloudOutputWriter::pushCloud(pcl::PointCloud<pcl::PointXYZRGB>::Const
 	cloudMeasurement.index = m_cloudCount;
 	m_clouds.push(cloudMeasurement);
 	m_cloudCount++;
-	if (m_cloudCount == numOfFilesToWrite){
-		m_running = false;
-	}
-	m_checkCloud.notify_one();
+	//if (m_cloudCount == numOfFilesToWrite){
+	//	m_running = false;
+	//}
+	m_checkCloud.notify_all();
 }
 
 void KinectCloudOutputWriter::startWritingClouds()
 {
 	const int threadsToStartCount = numOfFilesToWrite;
 	m_running = true;
-	for (int i = 0; i < threadsToStartCount; i++){
+	//5 threads => 17,71 secs
+	for (int i = 0; i < 5; i++){
 		m_writerThreads.push_back(std::thread(&KinectCloudOutputWriter::writeCloudToFile, this, i));
 	}
 }
 
+
+
 void KinectCloudOutputWriter::writeCloudToFile(int index)
 {
-	while (m_running)
+	bool cloudIsEmpty = false;
+	while (m_running || !cloudIsEmpty)
 	{
+
 		std::unique_lock<std::mutex> cloudLocker(m_lockCloud);
-		m_checkCloud.wait(cloudLocker);
-		
-		if (!m_clouds.empty())
-		{
-			auto cloudMeasurement = m_clouds.front();
-			std::stringstream fileName;
-			fileName << "Cloud_" << cloudMeasurement.index << ".ply";
-			m_clouds.pop();
-			cloudLocker.unlock();
-			pcl::io::savePLYFile(fileName.str(), *cloudMeasurement.cloud, true);
-			
+		while (m_clouds.empty() && m_running){
+			if (!m_checkCloud.wait_for(cloudLocker, std::chrono::milliseconds(100)) && !m_running){
+				return;
+			}
 		}
-		m_notified = false;
+		
+		if (m_clouds.empty()){
+			continue;
+		}
+		auto cloudMeasurement = m_clouds.front();
+		std::stringstream fileName;
+		fileName << "Cloud_" << cloudMeasurement.index << "_" << index << ".ply";
+		m_clouds.pop();
+		cloudIsEmpty = m_clouds.empty();
+		cloudLocker.unlock();
+
+		pcl::io::savePLYFile(fileName.str(), *cloudMeasurement.cloud, true);
 	}
 }
